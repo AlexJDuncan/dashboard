d21a6266fdd5d22074d9e3fd91474b8a5c5e9bbe
monkeymedic26 <monkeymedic26@gmail.com>
2022-09-12 08:35:36 -0600

    Add side menu and navbar styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
-import { BrowseRoute, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import './App.css';
 
 const App = () => {
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
@@ -21,6 +22,25 @@ const App = () => {
                 <FiSettings />
               </button>
             </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar
+            dark:bg-secondary-dark-bg
+            bg-white" >
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0
+            dark:bg-secondary-dark-bg">
+              Sidebar
+            </div>
+          )}
+          <div className={
+            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+          }>
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+            Navbar
+          </div>
           </div>
         </div>
       </BrowserRouter>
